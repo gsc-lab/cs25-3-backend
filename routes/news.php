ee1fe09dd5942e7178d570b7f51eff11dac4b960
HARUNA <166793731+TANAKAHARUNA7@users.noreply.github.com>
2025-11-11 01:25:35 +0900

    feature (news): Add news.php (#26)

--- routes/news.php
+++ routes/news.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 function registerNews(AltoRouter $router) :void {
     $router->map("GET", '/news', "NewsController#index"); // news 전체 보기
-    $router->map("GET", '/news/[i:news_id]', "NewsController#index"); // news 상세 보기
+    $router->map("GET", '/news/[a:news_id]', "NewsController#show"); // news 상세 보기
     $router->map("POST", '/news', "NewsController#create"); // news작성
-    $router->map("PUT", '/news/[i:news_id]', "NewsController#update"); // news수정 
-    $router->map("DELETE", '/news/[i:news_id]', "NewsController#delete"); // news 삭게하기    
+    $router->map("PUT", '/news/[a:news_id]', "NewsController#update"); // news수정 
+    $router->map("DELETE", '/news/[a:news_id]', "NewsController#delete"); // news 삭게하기    
 }
 
 ?>
