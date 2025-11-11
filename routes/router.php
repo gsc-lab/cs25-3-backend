36d1eabe3febe3a21fd124e3023bee1918eda110
HARUNA <166793731+TANAKAHARUNA7@users.noreply.github.com>
2025-11-12 01:27:20 +0900

    feature (router):update router implement (#33)

--- routes/router.php
+++ routes/router.php
@@ -3,9 +3,13 @@ declare(strict_types=1);
 
 require_once __DIR__.'/users.php';
 require_once __DIR__.'/news.php';
+require_once __DIR__.'/service.php';
+require_once __DIR__.'/designer.php';
 
 function registerAllRoutes(AltoRouter $router): void {
     $router->map('GET', '/', fn() => 'Home Page');
     registerUsers($router);
     registerNews($router);
+    registerService($router);
+    registerDesigner($router);
 }
