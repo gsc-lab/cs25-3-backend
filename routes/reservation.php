b9e74ee3be616749dfd860eebd9ad973811fe842
HARUNA <166793731+TANAKAHARUNA7@users.noreply.github.com>
2025-11-15 23:49:10 +0900

    feature(reservation):Add reservation.php (#61)

---
+++ routes/reservation.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+// 회원 가입 
+function registerReservation(AltoRouter $router): void {
+    $router->map('GET', "/reservation/[a:account]", 'ReservationController#show'); // (클라이언트) 자기 예약 정보 보기
+    $router->map('POST', "/reservation/create", 'ReservationController#create'); // 예약하기
+    $router->map('PUT', "/reservation/update/[a:account]", 'ReservationController#update'); // (클라이언트) 예약 cancel
+}
+
+?>
