d0eb6f09dae23cb59ded9586c29773b24d77f79d
HARUNA <166793731+TANAKAHARUNA7@users.noreply.github.com>
2025-11-15 22:53:11 +0900

    feature(salon): Update salon.php (#51)

--- routes/salon.php
+++ routes/salon.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 // salon
 function registerSalon(AltoRouter $router): void {
     $router->map('GET', "/salon", 'SalonController#index'); // 정보 보기
-    $router->map('PUT', "/salon", 'SalonController#update'); // salon 정보 수정
+    $router->map('PUT', "/salon/update", 'SalonController#update'); // salon 정보 수정
 }
 
 ?>
