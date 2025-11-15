3fb0bd57507c665b0c87d191b983fbfff99965b5
HARUNA <166793731+TANAKAHARUNA7@users.noreply.github.com>
2025-11-15 23:27:13 +0900

    feature(designer): Update designer.php (#57)

--- routes/designer.php
+++ routes/designer.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 function registerDesigner(AltoRouter $router):void{
     $router->map("GET", '/designer', "DesignerController#index"); // Designer정보 전체 보기
     $router->map("GET", '/designer/[a:designer_id]', "DesignerController#show"); // 해당하는 Designer정보 보기 
-    $router->map("POST", '/designer', "DesignerController#create"); // 작성하기
-    $router->map("PUT", '/designer/[a:designer_id]', "DesignerController#update"); // 해당하는 Designer정보 수정
-    $router->map("DELETE", '/designer/[a:designer_id]', "DesignerController#delete"); // 해당하는 Designer정보 삭제
+    $router->map("POST", '/designer/create', "DesignerController#create"); // 작성하기
+    $router->map("PUT", '/designer/update/[a:designer_id]', "DesignerController#update"); // 해당하는 Designer정보 수정
+    $router->map("DELETE", '/designer/delete/[a:designer_id]', "DesignerController#delete"); // 해당하는 Designer정보 삭제
 }
 ?>
