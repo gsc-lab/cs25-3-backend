ad397bd3ad87a1d4f9d8bf0a32aebe873737c18e
HARUNA <166793731+TANAKAHARUNA7@users.noreply.github.com>
2025-11-16 16:56:03 +0900

    feature(service): Update service.php (#63) (#64)

--- routes/service.php
+++ routes/service.php
@@ -3,8 +3,8 @@
 
     function registerService(AltoRouter $router) :void {
         $router->map('GET', "/service",'ServiceController#index');
-        $router->map('POST', "/service", 'ServiceController#create');
-        $router->map('PUT', "/service/[a:service_id]",'ServiceController#update');
-        $router->map('DELETE',"/service/[a:service_id]", 'ServiceController#delete');
+        $router->map('POST', "/service/create", 'ServiceController#create');
+        $router->map('PUT', "/service/update/[a:service_id]",'ServiceController#update');
+        $router->map('DELETE',"/service/delete/[a:service_id]", 'ServiceController#delete');
     }
 ?>
