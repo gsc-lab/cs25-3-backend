3afe2327e1b8c910fe0a05a0008d05dc0a04dd6c
HARUNA <166793731+TANAKAHARUNA7@users.noreply.github.com>
2025-11-18 01:01:59 +0900

    feature(hairstyle): Update hairstyle.php (#74)

--- routes/hairstyle.php
+++ routes/hairstyle.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 function registerHairstyle(AltoRouter $router) :void {
     $router->map("GET", '/hairstyle', "HairstyleController#index"); // hairstyle 전체 보기
+    $router->map("GET", '/hairstyle/[a:hairstyle_id]', "HairstyleController#show");  // hairstyle 세부 보기
     $router->map("POST", '/hairstyle/create', "HairstyleController#create"); // hairstyle 작성
-    $router->map("PUT", '/hairstyle/update/[a:hairstyle_id]', "HairstyleController#update"); // 특정 hairstyle수정 
+    $router->map("POST", '/hairstyle/image/[a:hairstyle_id]', "HairstyleController#updateImage");  // hairstyle 이미지 수정
+    $router->map("PUT", '/hairstyle/update/[a:hairstyle_id]', "HairstyleController#update"); // 특정 hairstyle 수정
     $router->map("DELETE", '/hairstyle/delete/[a:hairstyle_id]', "HairstyleController#delete"); // 특정 hairstyle 삭게하기    
 }
 
