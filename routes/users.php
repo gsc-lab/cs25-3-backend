b6c6c816a15a74ea98a3e45791575e5df266fc5c
haruna <haruna0306@g.yju.ac.kr>
2025-11-19 23:04:36 +0900

    feature(users): Update users.php

--- routes/users.php
+++ routes/users.php
@@ -3,14 +3,59 @@ declare(strict_types=1);
 
 // 회원 가입 
 function registerUsers(AltoRouter $router): void {
-    $router->map('GET', "/users/[a:account]", 'UsersController#show'); // 정보 보기
-    $router->map('POST', "/users/create", 'UsersController#create'); // 회원 가입
-    $router->map('PUT', "/users/update/[a:account]", 'UsersController#update'); // 회원 정보 수정
-    $router->map('DELETE', "/users/delete/[a:account]", 'UsersController#delete'); // 회원 탈퇴
-    $router->map('POST', "/users/login", 'UsersController#login'); // 로그인
-    $router->map('DELETE', "/users/logout", 'UsersController#logout'); // 로그아웃
+    
+    // ========================
+    // 회원 정보 보기 (login필수)
+    // ========================
+    $router->map('GET', "/users/[a:account]",
+     [  'controller' => 'UsersController',
+                'action'     => 'show',
+                'middleware' => ['login']
+            ]);
+    
+    // ================
+    // 회원 가입 (guest)
+    // ================
+    $router->map('POST', "/users/create",
+    [   'controller' => 'UsersController',
+                'action'     => 'create',
+                'middleware' => ['guest']
+            ]); 
+    
+    // =============
+    // 회원 정보 수정 
+    // =============
+    $router->map('PUT', "/users/update/[a:account]",
+    [   'controller' => 'UsersController',
+                'action'     => 'update',
+                'middleware' => ['login']
+            ]);
+
+    // ============================
+    // 회원 탈퇴 (login필수)(client 만)
+    // ============================
+    $router->map('DELETE', "/users/delete/[a:account]",
+     [  'controller' => 'UsersController',
+                'action'     => 'delete',
+                'middleware' => ['login', 'client']
+            ]); 
+            
+    // ==============
+    // 로그인 (guest)
+    // ==============
+    $router->map('POST', "/users/login",
+     [  'controller' => 'UsersController',
+                'action'     => 'login',
+                'middleware' => ['guest']
+            ]);  
+    
+    // ==================
+    // 로그아웃(login필수) 
+    // ==================
+    $router->map('DELETE', "/users/logout", 
+    [   'controller' => 'UsersController',
+                'action'     => 'logout',
+                'middleware' => ['login']]); 
 }
-
-
 
 ?>
