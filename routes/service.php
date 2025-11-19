0cde6aa2763ec3bcca93243c5c21f5f638b1c4b0
HARUNA <166793731+TANAKAHARUNA7@users.noreply.github.com>
2025-11-19 19:54:39 +0900

    feature(service): Update service.php (#76)

--- routes/service.php
+++ routes/service.php
@@ -2,9 +2,41 @@
     declare(strict_types=1);
 
     function registerService(AltoRouter $router) :void {
-        $router->map('GET', "/service",'ServiceController#index');
-        $router->map('POST', "/service/create", 'ServiceController#create');
-        $router->map('PUT', "/service/update/[a:service_id]",'ServiceController#update');
-        $router->map('DELETE',"/service/delete/[a:service_id]", 'ServiceController#delete');
+        
+        // ==========================
+        // service 정보 보기 (누구나 ok)
+        // ==========================
+        $router->map('GET', "/service",
+            [   'controller' => 'ServiceController',
+                        'action'     => 'index',
+                        'middleware' => []
+                    ]); 
+        
+        // =================================
+        // service 정보 작성 (login, manager)
+        // =================================
+        $router->map('POST', "/service/create",
+         [  'controller' => 'ServiceController',
+                    'action'     => 'create',
+                    'middleware' => ['login', 'manager']
+                ]);
+
+        // ==================================
+        // service 정보 수성 (login, manager)
+        // ==================================
+        $router->map('PUT', "/service/update/[a:service_id]",
+        [   'controller' => 'ServiceController',
+                    'action'     => 'update',
+                    'middleware' => ['login', 'manager']
+                ]);
+
+        // =================================
+        // service 정보 삭제 (login, manager)
+        // =================================
+        $router->map('DELETE',"/service/delete/[a:service_id]",
+                [   'controller' =>  'ServiceController',
+                    'action'     => 'delete',
+                    'middleware' => ['login', 'manager']
+                ]);
     }
 ?>
