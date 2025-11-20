6c514a1aa83cdf8ddd65d4b6387252ebb1d33c20
haruna <haruna0306@g.yju.ac.kr>
2025-11-20 15:22:54 +0900

    feature(designer): Update designer.php

--- routes/designer.php
+++ routes/designer.php
@@ -3,10 +3,52 @@
 declare(strict_types=1);
 
 function registerDesigner(AltoRouter $router):void{
-    $router->map("GET", '/designer', "DesignerController#index"); // Designer정보 전체 보기
-    $router->map("GET", '/designer/[a:designer_id]', "DesignerController#show"); // 해당하는 Designer정보 보기 
-    $router->map("POST", '/designer/create', "DesignerController#create"); // 작성하기
-    $router->map("PUT", '/designer/update/[a:designer_id]', "DesignerController#update"); // 해당하는 Designer정보 수정
-    $router->map("DELETE", '/designer/delete/[a:designer_id]', "DesignerController#delete"); // 해당하는 Designer정보 삭제
+    
+    // ============================
+    // Designer정보 전체 보기 (누구나)
+    // ============================
+    $router->map("GET", '/designer', [
+                'controller' => 'DesignerController', 
+                'action'     => 'index',
+                'middleware' => []
+            ]);
+                
+    // // ===============================
+    // // 해당하는 Designer정보 보기 
+    // // ===============================
+    // $router->map("GET", '/designer/[a:designer_id]',
+    // [  'controller' => 'DesignerController',
+    //             'action'     => 'show',
+    //             'middleware' => ['login']
+    //         ]);
+            
+    // =============================================
+    // designer 프로필 작성하기 (login필수)(designer만)
+    // =============================================     
+    $router->map("POST", '/designer/create',[
+                'controller' => 'DesignerController',
+                'action'     => 'create',
+                'middleware' => ['login', 'designer']
+            ]);
+           
+    // ===============================================
+    // 해당하는 Designer정보 수정 (login필수)(designer만)
+    // ===============================================
+    $router->map("PUT", '/designer/update/[a:designer_id]',[
+                'controller' => 'DesignerController',
+                'action'     => 'update',
+                'middleware' => ['login', 'designer']
+            ]); 
+
+
+    // ===============================================
+    // 해당하는 Designer정보 삭제 (login필수)(manager만)
+    // ===============================================        
+    $router->map("DELETE", '/designer/delete/[a:designer_id]',[
+                'controller' => 'DesignerController',
+                'action'     => 'delete',
+                'middleware' => ['login', 'manager']
+            ]); // 해당하는 Designer정보 삭제
+
 }
 ?>
